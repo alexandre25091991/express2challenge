932786cf500e1245ccf6efab6bfcd20c522b4b92
alexandre ROUZIERE <alexandre.rouziere1@gmail.com>
2023-05-12 11:43:59 +0200

    challenge6

--- app.js
+++ app.js
@@ -19,6 +19,7 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 app.post("/api/movies", movieHandlers.postMovie);
 app.put("/api/movies/:id", movieHandlers.updateMovie);
 app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.delete("/api/movies/:id", movieHandlers.deleteMovie);
 
 const userHandlers = require("./userHandlers");
 
